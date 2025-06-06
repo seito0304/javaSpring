7f0ce1ed814ec552246632d89c83e2ba9a525d55
seito0304 <iseito0304@outlook.com>
2025-06-06 09:58:45 +0900

    Delete ordersysdb directory

--- ordersysdb/src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
